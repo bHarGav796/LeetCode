bcdbe4e016af40cdc6a86ad0b799d7ef1a768121
bHarGav796 <phukan.bhargav796@gmail.com>
2023-07-24 12:06:53 +0530

    Revert "fifth commit"
    This reverts commit a5b12135a8aa5ebe26a2ff6b49a2e0133713e05f.

--- Leet-88.MergeSortedArray/src/main/java/org/example/Main.java
+++ Leet-88.MergeSortedArray/src/main/java/org/example/Main.java
@@ -5,20 +5,20 @@ import java.util.ArrayList;
 //apply list
 public class Main {
     public static void main(String[] args) {
-        int nums1[] = {1, 2, 3, 0, 0, 0}, nums2[] = {2, 5, 6};
+        int nums1 [] = {1,2,3,0,0,0}, nums2[] = {2,5,6};
         int m = 3, n = 3; //given
 
         ArrayList<Integer> list = new ArrayList<>();
-        for (int i = 0; i < m; i++) {
+        for(int i =0; i<m; i++){
             list.add(nums1[i]);
         }
-        for (int j = 0; j < n; j++) {
+        for(int j = 0; j<n ; j++){
             list.add(nums2[j]);
         }
 
-        int[] nums3 = list.stream().sorted().mapToInt(Integer::intValue).toArray();
-        System.arraycopy(nums3, 0, nums1, 0, nums3.length - 1);
-        for (int itr : nums1) {
+        int[] nums3 = list.stream().sorted().mapToInt(Integer :: intValue).toArray();
+        System.arraycopy(nums3,0,nums1,0,nums3.length-1);
+        for(int itr : nums1){
             System.out.println(nums1[itr]);
         }
 
@@ -27,6 +27,7 @@ srcPos is the index from which copying starts.
 dest denotes the destination array
 destPos is the index from which the copied elements are placed in the destination array.
 length is the length of the subarray to be copied.*/
+
+
     }
 }
-
