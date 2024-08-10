c41bee032e7a3b1b7c0983277b3706e9d700680b
Bhargav Phukan <phukan.bhargav796@gmail.com>
2024-08-10 18:56:59 +0530

    10/08/2024

--- 33. Search in Rotated Sorted Array/src/Main.java
+++ 33. Search in Rotated Sorted Array/src/Main.java
@@ -1,24 +1,44 @@
 class Solution {
     public int search(int[] nums, int target) {
 
-        int index = 0;
+        int index = nums.length/2, updated = 0;
 
         for (int i = 0; i < nums.length-1; i++) {
             if(nums[i] > nums[i+1])
+            {
                 index = i;
+                updated = 1;
+            }
         }
 
         if(target == nums[index])
             return index;
 
-        for(int i = index; i < nums.length; i++){
-            if(target == nums[i])
-                return i;
+
+        if(updated == 1) {
+            if (target < nums[0])
+                for(int i = index; i < nums.length; i++){
+                    if(target == nums[i])
+                        return i;
+                }
+            else
+                for(int i = 0; i < index; i++){
+                    if(target == nums[i])
+                        return i;
+                }
         }
-        for(int i = 0; i < index; i++){
-            if(target == nums[i])
-                return i;
-        }
+        else
+        if(target > nums[index])
+            for(int i = index; i < nums.length; i++){
+                if(target == nums[i])
+                    return i;
+            }
+        else
+            for(int i = 0; i < index; i++){
+                if(target == nums[i])
+                    return i;
+            }
+
 
         return -1;
     }
