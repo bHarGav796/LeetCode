197ea54758abc1563537d24f49b0fabd7b285c02
Bhargav Phukan <phukan.bhargav796@gmail.com>
2024-08-18 23:34:06 +0530

    18/08/24

--- 153. Find Minimum in Rotated Sorted Array/src/Main.java
+++ 153. Find Minimum in Rotated Sorted Array/src/Main.java
@@ -1,8 +1,9 @@
 import java.util.Arrays;
 
-public class Main {
-    public static void main(String[] args) {
-        int[] nums = {4,5,6,7,0,1,2};
-
+class Solution {
+    public int findMin(int[] nums) {
+        Arrays.sort(nums);
+        int n=nums[0];
+        return n;
     }
 }
